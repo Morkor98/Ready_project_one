1f4a099a4806a80e3f37b71f26605a4bad04287b
Omar <omarlucky0@gmail.com>
2025-04-16 11:48:34 +0200

    Miglioramento layout dashboard, aggiunte animazioni, da fixare una piccolezza

--- js/Dashboard_Omar.js
+++ js/Dashboard_Omar.js
@@ -8,6 +8,7 @@ const dashboardTitle = document.getElementById('h2');
 const card = document.querySelector('.card');
 
 dashboard.addEventListener('click', function () {
+    //FIXME il carosello si muove mentre la dashboard è aperta
     if (!currentExpanded) {
         this.classList.add('dashboard-expanded');
         overlay.classList.remove('hidden');
@@ -21,23 +22,19 @@ dashboard.addEventListener('click', function () {
         closeExpanded();
     }
 });
-//FIXME il carosello si muove mentre la dashboard è aperta
 overlay.addEventListener('click', closeExpanded);
 function closeExpanded() {
     if (currentExpanded) {
-
         //Avvia il fade-out del testo
         content.classList.add('fade-out');
-
         //Aspetta che finisca la transizione, poi si chiude la dashboard
         setTimeout(() => {
             currentExpanded.classList.remove('dashboard-expanded');
             overlay.classList.add('hidden');
             currentExpanded = null;
-
             card.classList.remove('hidden');
             content.classList.add('hidden');
-            content.classList.remove('fade-out'); // Reset per la prossima apertura
+            content.classList.remove('fade-out'); //Reset per la prossima apertura
             dashboardTitle.classList.remove('hidden');
         }, 300); // tempo uguale alla transizione in CSS
     }
@@ -45,7 +42,7 @@ function closeExpanded() {
 function toggleHover(isHovered) {
     const card = document.querySelector('.card');
 
-    // Evita l'hover se la dashboard è aperta
+    //Se la dashboard è aperta, interrompe l'effetto hover
     if (currentExpanded) return;
     if (isHovered) {
         card.classList.add('hovered');
@@ -53,22 +50,21 @@ function toggleHover(isHovered) {
         card.classList.remove('hovered');
     }
 }
-
-//FIXME Scrivere la condizione per cui si è sia da telefono e da tablet (occhio al desktop che si scazza)
-//Se sono su smartphone o tablet, le animazioni saranno uguali.
-if (window.matchMedia("(max-width: 767px)").matches || "(min-width: 768px) and (max-width: 1024px)".matches) {
-    carousel.addEventListener('slide.bs.carousel', function (event) {
-        const comingSlideIndex = event.to;
-        const dashboardIndex = 1; //indice dashboard 0 based
-        const fadeDuration = 2000;
-
-        if (comingSlideIndex === dashboardIndex) {
-            dashboardTitle.classList.add('hidden'); // nasconde h2
-            setTimeout(() => {
-                dashboardTitle.classList.add('fade-out');
-            }, fadeDuration);
-        } else {
-            dashboardTitle.classList.remove('hidden'); // lo mostra di nuovo
-        }
-    });
-}
+//FIXME Il nome sparisce troppo velocemente
+if (
+    window.matchMedia("(max-width: 767px)").matches ||
+    window.matchMedia("(min-width: 768px) and (max-width: 1024px)").matches) {
+        carousel.addEventListener('slide.bs.carousel', function (event) {
+            const comingSlideIndex = event.to;
+            const dashboardIndex = 1; //Indice dashboard 0 based
+            const fadeDuration = 2000;
+            if (comingSlideIndex === dashboardIndex) {
+                dashboardTitle.classList.add('hidden'); //Nasconde h2
+                setTimeout(() => {
+                    dashboardTitle.classList.add('fade-out');
+                }, fadeDuration);
+            } else {
+                dashboardTitle.classList.remove('hidden'); // lo mostra di nuovo
+            }
+        });
+    }
