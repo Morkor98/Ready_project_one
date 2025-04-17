d220a0f9afa7505080f41ec3d4c311794192792d
Morkor98 <miki.marcucci@gmail.com>
2025-04-17 10:50:23 +0200

    Icipit view desktop con grid, import di bootstrap DA INTEGRARE IN INDEX

--- js/member-JS-Michele.js
+++ js/member-JS-Michele.js
@@ -1,7 +1,14 @@
 const placeholder = document.getElementById("Placeholder");
+const placeholder2 = document.getElementById("Placeholder2");
 const img = document.getElementById("img");
+const img2 = document.getElementById("img2");
 
 document.getElementById("imageToggle").addEventListener("click", ()=>{
     placeholder.classList.add("hidden");
     img.classList.remove("hidden");
+});
+
+document.getElementById("imageToggle2").addEventListener("click", ()=>{
+    placeholder2.classList.add("hidden");
+    img2.classList.remove("hidden");
 });
