41b6b8e1c8395b27332c657fbb5c1038524c2c12
Francesco Serli <serlifrancesco@gmail.com>
2025-04-28 10:18:20 +0200

    aggiunta footer funzionanante al progetto

---
+++ js/DarkMode.js
@@ -0,0 +1,21 @@
+const button = document.getElementById('toggle-theme');
+const dark_mode_test = document.body;
+const footer = document.getElementById('footer');
+const icon = button.querySelector('i');
+
+button.addEventListener('click', () => {
+    dark_mode_test.classList.toggle('dark-mode');
+    footer.classList.toggle('dark_mode');
+
+    // Cambia l'icona da luna a sole
+    if (dark_mode_test.classList.contains('dark-mode')) {
+        icon.classList.remove('fa-moon');
+        icon.classList.add('fa-sun');
+
+    } else {
+        icon.classList.remove('fa-sun');
+        icon.classList.add('fa-moon');
+
+    }
+});
+
