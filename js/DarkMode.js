4ae7d6459f8bbb546bf3622260061831bd8af8dd
Omar <omarlucky0@gmail.com>
2025-04-28 11:57:18 +0200

    Aggiunta voce 'Chi siamo'
    # Conflicts:
    #	css/style.css

--- js/DarkMode.js
+++ js/DarkMode.js
@@ -2,10 +2,12 @@ const button = document.getElementById('toggle-theme');
 const dark_mode_test = document.body;
 const footer = document.getElementById('footer');
 const icon = button.querySelector('i');
+const line=document.getElementById('separate-line');
 
 button.addEventListener('click', () => {
     dark_mode_test.classList.toggle('dark-mode');
     footer.classList.toggle('dark_mode');
+    line.classList.toggle('dark-mode');
 
     // Cambia l'icona da luna a sole
     if (dark_mode_test.classList.contains('dark-mode')) {
@@ -18,4 +20,3 @@ button.addEventListener('click', () => {
 
     }
 });
-
