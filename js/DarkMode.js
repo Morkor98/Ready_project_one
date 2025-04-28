935a35d5be93c6b81cf15a78d59253b6ea429fc3
Omar <omarlucky0@gmail.com>
2025-04-28 16:53:26 +0200

    Aggiunto testo pre footer, sistemato layout da mobile, manca animazione sul testo del meme

--- js/DarkMode.js
+++ js/DarkMode.js
@@ -4,12 +4,15 @@ const footer = document.getElementById('footer');
 const icon = button.querySelector('i');
 const line=document.getElementById('separate-line');
 const lineMeme=document.getElementById('separate-lineMeme');
+const pUnder=document.getElementById('paragraph_pre_footer');
+
 
 button.addEventListener('click', () => {
     dark_mode_test.classList.toggle('dark-mode');
     footer.classList.toggle('dark_mode');
     line.classList.toggle('dark-mode');
     lineMeme.classList.toggle('dark-mode');
+    pUnder.classList.toggle('dark-mode');
 
     // Cambia l'icona da luna a sole
     if (dark_mode_test.classList.contains('dark-mode')) {
