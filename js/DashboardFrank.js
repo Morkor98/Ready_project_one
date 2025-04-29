5612dd5abfd0f4c8d93962a931ceec1066330246
Poltry03 <serlifrancesco@gmail.com>
2025-04-29 17:36:22 +0200

    Dashboard frank desktop finita

--- js/DashboardFrank.js
+++ js/DashboardFrank.js
@@ -1,7 +1,20 @@
 const square=document.getElementById("square");
+const dash=document.querySelector(".name");
+let frank = null;
 
 
 document.getElementById("Dashboard_Frank_grid").addEventListener("click", function(){
-square.classList.add("click");
+square.classList.remove("disappear");
+dash.classList.add("disappear");
+frank = square;
 })
+
+
+document.addEventListener('click', () => {
+    if (frank) {
+        frank.classList.add('disappear');
+        dash.classList.remove('disappear');
+        frank = null;
+    }
+});
 
