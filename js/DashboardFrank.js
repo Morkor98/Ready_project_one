53c620859f631303bfd4cea902bba6d7747d92cd
Poltry03 <serlifrancesco@gmail.com>
2025-04-30 14:54:02 +0200

    fix finale con click annesso in modalità carusel dash frank

--- js/DashboardFrank.js
+++ js/DashboardFrank.js
@@ -1,5 +1,8 @@
 const square=document.getElementById("square");
 const dash=document.querySelector(".name");
+const image=document.querySelector(".image-wrapper");
+const image1=document.querySelector(".img1");
+const image2=document.querySelector(".img2");
 let frank = null;
 
 
@@ -9,7 +12,10 @@ dash.classList.add("disappear");
 frank = square;
 })
 
-
+image.addEventListener("click", function(){
+    image1.classList.toggle("click");
+    image2.classList.toggle("click1");
+})
 document.addEventListener('click', () => {
     if (frank) {
         frank.classList.add('disappear');
